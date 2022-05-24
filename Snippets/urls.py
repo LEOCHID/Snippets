8a1b94a0bfc309546f495002b051bf7c5206e4cf
username <darkesgg285@gmail.com>
2022-05-24 21:46:47 +0300

    Создали снипеты

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('', views.index_page, name="home"),
     path('snippets/add', views.add_snippet_page, name ="snippets_add"),
     path('snippets/list', views.snippets_page, name="snippets_list"),
+    path('snippets1', views.add_snippet_page, name ="snippets1"),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
